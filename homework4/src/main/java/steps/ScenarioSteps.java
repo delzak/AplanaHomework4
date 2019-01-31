4fed665c2498da581db6cf889ea0903265b7c9e9
delzak <lex_909@mail.ru>
2019-01-31 23:38:10 +0300

    При выполнении домашнего задания №4 тесты перестали запускаться и выкидывают ошибку

---
+++ homework4/src/main/java/steps/ScenarioSteps.java
@@ -0,0 +1,68 @@
+package steps;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by Алексей on 31.01.2019.
+ */
+public class ScenarioSteps {
+    MainSteps mainSteps = new MainSteps();
+    TravelAndShopingSteps travelAndShopingSteps = new TravelAndShopingSteps();
+    TravelInsuranceSteps travelInsuranceSteps = new TravelInsuranceSteps();
+
+    @When("^выбран пункт меню \"(.*)\"$")
+    public void stepSelectMainMenu(String menuItem) {
+        mainSteps.stepSelectMainMenu(menuItem);
+    }
+
+    @When("^выбран раздел \"(.*)\"$")
+    public void stepSelectSubMenu(String menuItem) {
+        mainSteps.stepSelectSubMenu(menuItem);
+    }
+
+    @When("^выполнена проверка наличия заголовка \"([^\"]*)\" на странице$")
+    public void stepFindTitle(String titleItem) {
+        travelAndShopingSteps.stepFindTitle(titleItem);
+    }
+
+    @When("^выполнено нажатие на кнопку - Оформить онлайн$")
+    public void stepClickRegistrationButton() {
+        travelAndShopingSteps.stepClickRegistrationButton();
+    }
+
+    @When("^выбран полис на сумму страховой защиты – Минимальная$")
+    public void stepClickMinInsurance() {
+        travelInsuranceSteps.stepClickMinInsurance();
+    }
+
+    @When("^выполнено нажатие кнопки - Оформить$")
+    public void stepClickIssueButton() {
+        travelInsuranceSteps.stepClickIssueButton();
+    }
+
+    @When("заполняются поля:")
+    public void stepFillFields(DataTable fields) {
+        fields.asMap(String.class, String.class).forEach(
+                (key, value) -> travelInsuranceSteps.stepFillField(key, value));
+    }
+
+    @When("проверяются поля:")
+    public void stepAssertFields(DataTable fields) {
+        fields.asMap(String.class, String.class).forEach(
+                (key, value) -> travelInsuranceSteps.stepAssertField(key, value));
+    }
+
+    @When("^выполнено нажатие кнопки - Продолжить$")
+    public void stepClickContinueButton() {
+        travelInsuranceSteps.stepClickContinueButton();
+    }
+
+    @When("^проверка появления сообщения об ошибке \"(.*)\"$")
+    public void stepAssertErrorText(String errorText) {
+        errorText = "Заполнены не все обязательные поля";
+        travelInsuranceSteps.stepAssertErrorText(errorText);
+    }
+
+
+}
---
+++ homework4/src/main/java/steps/TravelInsuranceSteps.java
@@ -0,0 +1,65 @@
+package steps;
+
+import pages.TravelInsurancePage;
+import ru.yandex.qatools.allure.annotations.Step;
+
+import java.util.HashMap;
+
+/**
+ * Created by Алексей on 28.01.2019.
+ */
+public class TravelInsuranceSteps extends BaseSteps {
+
+    String surname = "Surname";
+    String name = "Name";
+    String ruSurname = "Фамилия";
+    String ruName = "Имя";
+    String middlename = "Отчество";
+    String birthday = "01.01.1991";
+    String series = "1111";
+    String number = "222222";
+    String issueDay = "01.02.2012";
+    String issuedBy = "Выдан людьми из паспортного стола";
+
+    @Step("Поле {0} заполнено значением {1}")
+    public void stepFillField(String field, String value) {
+        new TravelInsurancePage().fillField(field, value);
+    }
+
+    @Step("Заполняются поля:")
+    public void stepFillFields(HashMap<String, String> fields) {
+        fields.forEach(this::stepFillField);
+    }
+
+    @Step("Проверить корректность поля {0} заполненным значением {1}")
+    public void stepAssertField(String field, String value) {
+        new TravelInsurancePage().assertFieldText(field, value);
+    }
+
+    @Step("Проверяются поля:")
+    public void stepAssertFields(HashMap<String, String> fields) {
+        fields.forEach(this::stepAssertField);
+    }
+
+    @Step("Выбор полиса на сумму страховой защиты – Минимальная")
+    public void stepClickMinInsurance() {
+        new TravelInsurancePage().minInsurance.click();
+    }
+
+    @Step("Нажатие кнопки 'Оформить'")
+    public void stepClickIssueButton() {
+        new TravelInsurancePage().issueButton.click();
+    }
+
+
+    @Step("Нажатие кнопки 'Продолжить'")
+    public void stepClickContinueButton() {
+        new TravelInsurancePage().continueButton.click();
+    }
+
+    @Step("Проверка появления сообщения об ошибке {0}")
+    public void stepAssertErrorText(String errorText) {
+        errorText = "Заполнены не все обязательные поля";
+        new TravelInsurancePage().assertFieldText("Сообщение об ошибке", errorText);
+    }
+}
